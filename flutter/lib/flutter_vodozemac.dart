997ce20096ad7c80a501c184bf14fb09a772bc85
Christian Kußowski <c.kussowski@famedly.com>
2025-06-09 07:43:55 +0200

    refactor: Pass wasmPath parameter

--- flutter/lib/flutter_vodozemac.dart
+++ flutter/lib/flutter_vodozemac.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:vodozemac/vodozemac.dart' as vod;
 
-Future<void> init() => vod.init(
-      wasmPath: './pkg/',
+Future<void> init({String wasmPath = './pkg/'}) => vod.init(
+      wasmPath: wasmPath,
       libraryPath: './',
       stem: !kIsWeb && (Platform.isIOS || Platform.isMacOS)
           ? 'flutter_vodozemac'
